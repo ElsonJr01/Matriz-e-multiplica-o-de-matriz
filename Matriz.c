a62d79f892e880c18d0e70d802fc4bec5478f88e
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2024-12-19 21:57:28 -0300

    Add files via upload
    multiplicação de matrizes de ordem 4x4 e 4x3, sendo o resultado uma matriz 4x3

--- Matriz.c
+++ Matriz.c
@@ -3,18 +3,28 @@
 int main(){
 	
 	int matriz[4][4];
+	int mat[4][3];
 	int i;
 	int j;
 	
     for(i=0; i<4; i++){
     	for(j=0; j<4; j++){
-    		printf("\nDigite o valor que esta na linha %d, coluna %d: ", i, j);
+    		printf("\nDigite o valor que esta na linha %d, coluna %d da primeira matriz: ", i, j);
     		scanf("%d", &matriz[i][j]);
 		}
 	}
+	
+	for(i=0; i<4; i++){
+		for(j=0; j<3; j++){
+			printf("\nDigite o valor que esta na linha %d, coluna %d da segunda matriz: ", i, j);
+			scanf("%d",&mat[i][j]);
+		}
+	}
+	
+	
 	for(i=0; i<4; i++){
 		for(j=0; j<4; j++){
-			matriz[i][j] = matriz[i][j] * 3;
+			matriz[i][j] = matriz[i][j] * mat[i][j];
 		}
 	}
 
